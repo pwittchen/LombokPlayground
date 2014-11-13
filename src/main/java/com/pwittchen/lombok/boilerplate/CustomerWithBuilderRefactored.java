56d3f55527868f731b2df8432d50e2d1113369dd
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-11-14 00:04:42 +0100

    Updating refactored class with builder

--- src/main/java/com/pwittchen/lombok/boilerplate/CustomerWithBuilderRefactored.java
+++ src/main/java/com/pwittchen/lombok/boilerplate/CustomerWithBuilderRefactored.java
@@ -15,4 +15,8 @@ public class CustomerWithBuilderRefactored {
     String email;
     String address;
     List<String> products;
+
+    public String getNameWithUpperCase() {
+        return name.toUpperCase();
+    }
 }
